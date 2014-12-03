488a8d2757f6ddcf41655ee3979e64d46e049a95
Ashley Pond V <ashley@cpan.org>
2014-12-02 16:24:42 -0800

    Disclaimer boilerplate.

--- lib/Wren.pm
+++ lib/Wren.pm
@@ -188,6 +188,7 @@ L<http://github.com/pangyre/wren>.
 
 =head1 See Also
 
+...
 
 =head1 Author
 
@@ -197,6 +198,29 @@ Ashley Pond V E<middot> ashley@cpan.org.
 
 Artistic 2.0.
 
+=head1 Disclaimer of Warranty
+
+Because this software is licensed free of charge, there is no warranty
+for the software, to the extent permitted by applicable law. Except when
+otherwise stated in writing the copyright holders and other parties
+provide the software "as is" without warranty of any kind, either
+expressed or implied, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose. The
+entire risk as to the quality and performance of the software is with
+you. Should the software prove defective, you assume the cost of all
+necessary servicing, repair, or correction.
+
+In no event unless required by applicable law or agreed to in writing
+will any copyright holder, or any other party who may modify or
+redistribute the software as permitted by the above license, be
+liable to you for damages, including any general, special, incidental,
+or consequential damages arising out of the use or inability to use
+the software (including but not limited to loss of data or data being
+rendered inaccurate or losses sustained by you or third parties or a
+failure of the software to operate with any other software), even if
+such holder or other party has been advised of the possibility of
+such damages.
+
 =cut
 
  #  ID       QS     Content-Type   Encoding Char-Set        Lang   Size
@@ -204,4 +228,6 @@ Artistic 2.0.
    ['var2',  0.950, 'text/plain',  'gzip',  'us-ascii',     'no',    400],
    ['var3',  0.3,   'image/gif',   undef,   undef,          undef, 43555],
   ];
+
+
 
